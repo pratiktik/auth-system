5c33e9d2c0acfe826be2dba746fdfbfb1c1f3725
Pratiktik <pratikshekhare@gmail.com>
2026-05-03 07:46:19 +0530

    Initial auth project setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Auth server running');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
